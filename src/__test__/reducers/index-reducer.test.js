f6a58c1870c274eb8c6d8d68f8fead8310a9de5d
Peter Grimm <pagrimm@gmail.com>
2020-09-18 13:54:37 -0700

    add passing tests for index reducer

---
+++ src/__test__/reducers/index-reducer.test.js
@@ -0,0 +1,47 @@
+import rootReducer from '../../reducers/index';
+import {createStore} from 'redux';
+import formVisibleReducer from '../../reducers/form-visible-reducer';
+import kegListReducer from '../../reducers/keg-list-reducer';
+
+let store = createStore(rootReducer);
+
+describe("rootReducer", () => {
+
+  test('Should return default state if no action type is recognized', () => {
+    expect(rootReducer({}, {type:null})).toEqual({
+      masterKegList: {},
+      formVisibleOnPage: false
+    });
+  });
+  
+  test('Check that initial state of kegListReducer matches root reducer', () => {
+    expect(store.getState().masterKegList).toEqual(kegListReducer(undefined, { type: null }));
+  });
+
+  test('Check that initial state of formVisibleReducer matches rootReducer', () => {
+    expect(store.getState().formVisibleOnPage).toEqual(formVisibleReducer(undefined, { type: null }));
+  });
+
+  test('Check that state of kegListReducer matches rootReducer', () => {
+    const action = {
+      type: 'ADD_KEG',
+      name: 'Barleybrew Scalder',
+      brand: 'Barleybrew Brewery',
+      price: 10,
+      alcohol: 30,
+      pints: 124,
+      id: 1,
+    };
+    store.dispatch(action);
+    expect(store.getState().masterKegList).toEqual(kegListReducer(undefined, action));
+  });
+
+  test('Check that state of formVisibleReducer matches rootReducer', () => {
+    const action = {
+      type: 'TOGGLE_FORM'
+    }
+    store.dispatch(action);
+    expect(store.getState().formVisibleOnPage).toEqual(formVisibleReducer(undefined, action));
+  });
+
+});
