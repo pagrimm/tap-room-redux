60ec8e8dbe69db40c58874575a7da5965b5e4e10
Peter Grimm <pagrimm@gmail.com>
2020-09-18 13:04:32 -0700

    add failing test for adding keg to keg list in keg list reducer

--- src/__test__/reducers/keg-list-reducer.test.js
+++ src/__test__/reducers/keg-list-reducer.test.js
@@ -1,9 +1,42 @@
 import kegListReducer from '../../reducers/keg-list-reducer';
 
 describe('kegListReducer', () => {
+  let action;
+  const kegData = {
+    name: 'Barleybrew Scalder',
+    brand: 'Barleybrew Brewery',
+    price: 10,
+    alcohol: 30,
+    pints: 124,
+    id: 1,
+  };
 
   test('Should return default state if no action type passed into reducer', () => {
     expect(kegListReducer({}, {type: null})).toEqual({});
   });
 
+  test('Should successfully add keg to keg list', () => {
+    const { name, brand, price, alcohol, pints, id} = kegData;
+    action = {
+      type: 'ADD_POST',
+      name: name,
+      brand: brand,
+      price: price,
+      alcohol: alcohol,
+      pints: pints,
+      id: id
+    };
+    expect(kegListReducer({}, action)).toEqual({
+      [id] : {
+        name: name,
+        brand: brand,
+        price: price,
+        alcohol: alcohol,
+        pints: pints,
+        id: id
+      }
+    });
+  });
+
+  
 });
